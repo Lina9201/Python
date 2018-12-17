2ff9cd2f5f06378b0c1cbf507dc615a6532c7a9a
Lina9201 <lina.zhu@outlook.com>
2018-12-17 17:40:41 +0800

    2018-12-17提交

--- TCRCInterface/base/SendRequest.py
+++ TCRCInterface/base/SendRequest.py
@@ -1,8 +1,6 @@
-from TCRCInterface.base.runMethod import RunMethod
 from TCRCInterface.util.operation_excel import Operation_excel
 from TCRCInterface.data.dependent_data import dependent_data
 import requests
-import json
 def send_requests(s, testdata):
         '''封装requests请求'''
         method = testdata["请求方式"]
@@ -12,26 +10,38 @@ def send_requests(s, testdata):
         test_nub = testdata['ID']
         expect = eval(testdata['预期结果'])
         depend_case = testdata['依赖id']
-        # if depend_case !=None:
-        #     depend_data = dependent_data(depend_case)
-        #     depend_response_data = depend_data.get_for_key(1)
-        #     depend_data = eval(testdata['依赖数据所属字段'])
-        #     header[depend_data] = depend_response_data
-        #     res = s.request(method=method, url=url, headers=header, data=json.dumps(data))
-        #     print("*******正在执行用例：-----  %s  ----**********" % test_nub)
-        #     print("请求方式：%s, 请求url:%s" % (method, url))
-        #     print("预期结果: %s" % expect)
-        #     print("依赖ID: %s" % depend_case)
-        #     return res
-        # else:
-        print("*******正在执行用例：-----  %s  ----**********" % test_nub)
-        print("请求方式：%s, 请求url:%s" % (method, url))
-        print("预期结果: %s" % expect)
-        print("依赖ID: %s" % depend_case)
-        res = s.request(method=method, url=url, headers=header, data=data)
-        print(res)
-        print(res.text)
-        return res
+        if depend_case == "":
+            print("*******正在执行用例：-----  %s  ----**********" % test_nub)
+            print("请求方式：%s, 请求url:%s" % (method, url))
+            print("预期结果: %s" % expect)
+            print("依赖ID: %s" % depend_case)
+            res = s.request(method=method, url=url, headers=header, data=data)
+            print(res)
+            print(res.text)
+            return res
+        else:
+            # depend_data = dependent_data("TCRC_001")
+            # depend_response_data = depend_data.get_for_key(1)
+            # depend_data = eval(testdata['依赖数据所属字段'])
+            # header[depend_data] = depend_response_data
+            # res = s.request(method=method, url=url, headers=header, data=json.dumps(data))
+            print("*******正在执行用例：-----  %s  ----**********" % test_nub)
+            print("请求方式：%s, 请求url:%s" % (method, url))
+            print("预期结果: %s" % expect)
+            # print("依赖ID: %s" % depend_case)
+            # print("依赖ID: %s" % depend_data)
+            # print("依赖ID: %s" % depend_response_data)
+            # print(res.text)
+            # return res
+
+        # print("*******正在执行用例：-----  %s  ----**********" % test_nub)
+        # print("请求方式：%s, 请求url:%s" % (method, url))
+        # print("预期结果: %s" % expect)
+        # print("依赖ID: %s" % depend_case)
+        # res = s.request(method=method, url=url, headers=header, data=data)
+        # print(res)
+        # print(res.text)
+        # return res
 
 if __name__ == "__main__":
     data = Operation_excel().dict_data()
