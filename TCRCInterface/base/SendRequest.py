b75f9560e97e3518f1a3210f39fb7204a415a2b9
Lina9201 <lina.zhu@outlook.com>
2018-12-18 15:44:14 +0800

    2018-12-17提交

--- TCRCInterface/base/SendRequest.py
+++ TCRCInterface/base/SendRequest.py
@@ -1,6 +1,8 @@
 from TCRCInterface.util.operation_excel import Operation_excel
 from TCRCInterface.data.dependent_data import dependent_data
 import requests
+import json
+from jsonpath_rw import jsonpath, parse
 def send_requests(s, testdata):
         '''封装requests请求'''
         method = testdata["请求方式"]
@@ -10,29 +12,30 @@ def send_requests(s, testdata):
         test_nub = testdata['ID']
         expect = eval(testdata['预期结果'])
         depend_case = testdata['依赖id']
+        depend_data = testdata['依赖数据']
         if depend_case == "":
             print("*******正在执行用例：-----  %s  ----**********" % test_nub)
             print("请求方式：%s, 请求url:%s" % (method, url))
             print("预期结果: %s" % expect)
-            print("依赖ID: %s" % depend_case)
             res = s.request(method=method, url=url, headers=header, data=data)
-            print(res)
-            print(res.text)
-            return res
+            print("请求响应结果: %s" % res.text)
         else:
-            # depend_data = dependent_data("TCRC_001")
-            # depend_response_data = depend_data.get_for_key(1)
-            # depend_data = eval(testdata['依赖数据所属字段'])
-            # header[depend_data] = depend_response_data
-            # res = s.request(method=method, url=url, headers=header, data=json.dumps(data))
+            dependdata = dependent_data("TCRC_001")
+            depend_response_data = dependdata.run_dependent()
+            jsonpath_expr = parse(depend_data)
+            madle = jsonpath_expr.find(depend_response_data)
+            print(madle)
+            depend_response_data_new = [match.value for match in madle][0]
+            depend_data_new = testdata['依赖数据所属字段']
+            header[depend_data_new] = depend_response_data_new
+            res = s.request(method=method, url=url, headers=header, data=json.dumps(data))
             print("*******正在执行用例：-----  %s  ----**********" % test_nub)
             print("请求方式：%s, 请求url:%s" % (method, url))
             print("预期结果: %s" % expect)
-            # print("依赖ID: %s" % depend_case)
-            # print("依赖ID: %s" % depend_data)
-            # print("依赖ID: %s" % depend_response_data)
-            # print(res.text)
-            # return res
+            print("依赖ID: %s" % depend_case)
+            print("依赖数据: %s" % depend_data)
+            print("请求响应结果: %s" % res.text)
+            return res
 
         # print("*******正在执行用例：-----  %s  ----**********" % test_nub)
         # print("请求方式：%s, 请求url:%s" % (method, url))
